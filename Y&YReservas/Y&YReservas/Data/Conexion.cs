36317a06c36191b7596d4633edd841fe142660e6
yvasquez23 <yvasquez@servempsys.com>
2025-10-01 12:40:40 -0400

    Creacion de la primera pantalla

---
+++ Y&YReservas/Y&YReservas/Data/Conexion.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace Y_YReservas.Data
+{
+   public class Conexion
+    {
+        public static string COnexion()
+        {
+            return ConfigurationManager.ConnectionStrings["SQLserver"].ConnectionString;
+        }
+    }
+}
